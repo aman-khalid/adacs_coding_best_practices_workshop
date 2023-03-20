1eb632c6123d38f7d7e5b0c62990917d48287665
Aman Khalid <71977108+aman-khalid@users.noreply.github.com>
2023-03-20 15:14:54 +1100

    DRY principle restructuring of code

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,37 +1,51 @@
-''' A script to simulate a catalogue of simulated stars around Andromeda galaxy '''
+#! /usr/bin/env python
+''' 
+A script to simulate a catalogue of simulated stars around Andromeda galaxy 
+Author: Aman Khalid
+Last Change: 20/03/23
+'''
 import math as mth
 import random as rand
 import matplotlib.pyplot as plt
-# Determine Andromeda location in ra/dec degrees
 
-# from wikipedia
-RA = '00:42:44.3'
-DEC = '41:16:09'
+NSRC=1_000_000
 
-# convert to decimal degrees
-d, m, s = DEC.split(':')
-DEC = int(d)+int(m)/60+float(s)/3600
+def get_ra_dec():
+    # Determine Andromeda location in ra/dec degrees
+    # from wikipedia
+    RA = '00:42:44.3'
+    DEC = '41:16:09'
 
-h, m, s = RA.split(':')
-RA = 15*(int(h)+int(m)/60+float(s)/3600)
-RA = RA/mth.cos(DEC*mth.pi/180)
+    # convert to decimal degrees
+    d, m, s = DEC.split(':')
+    dec = int(d)+int(m)/60+float(s)/3600
 
-# make 1000 stars within 1 degree of Andromeda
-NSRC = 1_000_000
+    h, m, s = RA.split(':')
+    ra = 15*(int(h)+int(m)/60+float(s)/3600)
+    ra = ra/mth.cos(dec*mth.pi/180)
+    
+    return (ra, dec)
 
-ras = []
-decs = []
-for i in range(NSRC):
-    ras.append(RA + rand.uniform(-1,1))
-    decs.append(DEC + rand.uniform(-1,1))
+def make_stars(ra, dec, NSRC):
+    # make 1,000,000 stars within 1 degree of Andromeda
 
-# now write these to a csv file for use by my other program
-with open('./catalog.csv','w') as f:
-    print("id,ra,dec", file=f)
+    ras = []
+    decs = []
     for i in range(NSRC):
-        print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
+        ras.append(ra + rand.uniform(-1,1))
+        decs.append(dec + rand.uniform(-1,1))
+    return (ras, decs)
 
-plt.scatter(ras[::1000], decs[::1000])
-plt.xlabel('RA [deg]')
-plt.ylabel('DEC [deg]')
-plt.savefig('./plots/sim_star_positions_plot.png')
+if __name__=='__main__':
+    ra, dec = get_ra_dec()
+    ras, decs = make_stars(ra, dec, NSRC)
+    # now write these to a csv file for use by my other program
+    with open('./catalog.csv','w', encoding='utf-8') as f:
+        print("id,ra,dec", file=f)
+        for i in range(NSRC):
+            f.write(F"{i:07d}, {ras[i]:12f}, {decs[i]:12f}\n")
+
+    plt.scatter(ras[::1000], decs[::1000])
+    plt.xlabel('RA [deg]')
+    plt.ylabel('DEC [deg]')
+    plt.savefig('./plots/sim_star_positions_plot.png')
