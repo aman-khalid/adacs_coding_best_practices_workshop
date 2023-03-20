5c9ad34cbfb991f8c368addd03a87475f68bd0a9
Aman Khalid <71977108+aman-khalid@users.noreply.github.com>
2023-03-20 13:52:21 +1100

    tidying up code

--- mymodule/sky_sim.py
+++ mymodule/sky_sim.py
@@ -1,29 +1,29 @@
-# Determine Andromeda location in ra/dec degrees
 ''' A script to simulate a catalogue of simulated stars around Andromeda galaxy '''
+import math as mth 
+import random as rand
+import matplotlib.pyplot as plt
+# Determine Andromeda location in ra/dec degrees
+
 # from wikipedia
 ra = '00:42:44.3'
 dec = '41:16:09'
 
 # convert to decimal degrees
-from math import *
-
 d, m, s = dec.split(':')
 dec = int(d)+int(m)/60+float(s)/3600
 
 h, m, s = ra.split(':')
 ra = 15*(int(h)+int(m)/60+float(s)/3600)
-ra = ra/cos(dec*pi/180)
-
-nsrc = 1_000_000
+ra = ra/mth.cos(dec*mth.pi/180)
 
 # make 1000 stars within 1 degree of Andromeda
-from random import *
+nsrc = 1_000_000
+
 ras = []
 decs = []
 for i in range(nsrc):
-    ras.append(ra + uniform(-1,1))
-    decs.append(dec + uniform(-1,1))
-
+    ras.append(ra + rand.uniform(-1,1))
+    decs.append(dec + rand.uniform(-1,1))
 
 # now write these to a csv file for use by my other program
 f = open('../data/catalog.csv','w')
@@ -31,7 +31,6 @@ print("id,ra,dec", file=f)
 for i in range(nsrc):
     print("{0:07d}, {1:12f}, {2:12f}".format(i, ras[i], decs[i]), file=f)
 
-import matplotlib.pyplot as plt
 plt.scatter(ras[::1000], decs[::1000])
 plt.xlabel('RA [deg]')
 plt.ylabel('DEC [deg]')
